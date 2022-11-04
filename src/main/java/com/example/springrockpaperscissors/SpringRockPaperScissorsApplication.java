8470f0517c42678f75afb80b950953c6bde6da80
Balazs2014 <tbalazs.levente@gmail.com>
2022-11-04 15:22:36 +0100

    Projekt létrehozása

---
+++ src/main/java/com/example/springrockpaperscissors/SpringRockPaperScissorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrockpaperscissors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRockPaperScissorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRockPaperScissorsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springrockpaperscissors/SpringRockPaperScissorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrockpaperscissors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRockPaperScissorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
